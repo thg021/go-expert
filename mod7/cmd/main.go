22df29adaef4d568ae0718deecc4b8457817cb1a
Thiago Silva <thg021@gmail.com>
2023-01-24 02:50:35 +0000

    feat: module math

---
+++ mod7/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thg021/go-expert/mod7/math"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m)
+}
---
+++ mod7/go.mod
@@ -0,0 +1,3 @@
+module github.com/thg021/goexpert/mod7/math
+
+go 1.19
